1a20b0d95ff5e91ed7ab67249ab8c5bcccb8dd4d
Nataliya Shostak <slanchevbryag@yandex.ru>
2024-09-15 21:58:55 +0300

    Done

--- 5_while2.py
+++ 5_while2.py
@@ -15,13 +15,22 @@
     
 """
 
-questions_and_answers = {}
+questions_and_answers = {'Привет': 'Привет',
+                         'Как дела': 'Хорошо',
+                         'Что делаешь': 'Программирую'}
 
 def ask_user(answers_dict):
     """
     Замените pass на ваш код
     """
-    pass
+    while True:
+        
+        bla_bla = input('Поболтаем? ')
+
+        if bla_bla == 'Пока':
+            print('Ариведерчи')
+            break
+        print(questions_and_answers.get(bla_bla))
     
 if __name__ == "__main__":
     ask_user(questions_and_answers)
