8972246772624ccae8c34e16f6e7f9fd55003fea
Nataliya Shostak <slanchevbryag@yandex.ru>
2024-09-15 23:07:40 +0300

    Done

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,13 +13,27 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
+def discounted(price, discount, max_discount=20):
     """
     Замените pass на ваш код
     """
-    pass
+    try:
+      price = abs(float(price))
+      discount = abs(float(discount))
+      max_discount = abs(int(max_discount))
     
+      if max_discount >= 100:
+          raise ValueError('Слишком большая максимальная скидка')
+      if discount >= max_discount:
+          return price
+      else:
+          return price - (price * discount / 100)
+      
+    except (ValueError, TypeError):
+      print('Введите число') 
+
 if __name__ == "__main__":
+    
     print(discounted(100, 2))
     print(discounted(100, "3"))
     print(discounted("100", "4.5"))
